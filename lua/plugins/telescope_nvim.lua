c2166bc6705121a0667e00a4e3ca5d240331cb1f
tamago324 <tamago_pad@yahoo.co.jp>
2021-02-23 16:18:57 +0900

    update

--- lua/plugins/telescope_nvim.lua
+++ lua/plugins/telescope_nvim.lua
@@ -8,6 +8,8 @@ local previewers = require('telescope.previewers')
 -- local conf = require('telescope.config').values
 -- local transform_mod = require('telescope.actions.mt').transform_mod
 -- local make_entry = require('telescope.make_entry')
+local entry_display = require('telescope.pickers.entry_display')
+local devicons = require'nvim-web-devicons'
 
 local Path = require'plenary.path'
 
@@ -301,8 +303,97 @@ end
 -- @Summary buffers
 -- @Description
 local buffers = function()
+  -- 表示したくないバッファ
+  local invalid_regex_list = {'^term://', '^deol%-edit@'}
+
+  local is_valid_bufnr = function(bufnr)
+    for _, re in ipairs(invalid_regex_list) do
+      if vim.api.nvim_buf_get_name(bufnr):match(re) then
+        return false
+      end
+    end
+    return true
+  end
+
+  local gen_from_buffer_like_leaderf = function(opts)
+    opts = opts or {}
+    local default_icons, _ = devicons.get_icon('file', '', {default = true})
+
+    local bufnrs = vim.tbl_filter(function(b)
+      return 1 == vim.fn.buflisted(b) and is_valid_bufnr(b)
+    end, vim.api.nvim_list_bufs())
+
+    local max_bufnr = math.max(unpack(bufnrs))
+    local bufnr_width = #tostring(max_bufnr)
+
+    local max_bufname = math.max(
+      unpack(
+        vim.tbl_map(function(bufnr)
+          return vim.fn.strdisplaywidth(vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ':p:t'))
+        end, bufnrs)
+      )
+    )
+
+    local displayer = entry_display.create {
+      separator = " ",
+      items = {
+        { width = bufnr_width },
+        { width = 4 },
+        { width = vim.fn.strwidth(default_icons) },
+        { width = max_bufname },
+        { remaining = true },
+      },
+    }
+
+    -- local cwd = vim.fn.expand(opts.cwd or vim.fn.getcwd())
+
+    -- リストの場合、ハイライトする
+    local make_display = function(entry)
+      return displayer {
+        {entry.bufnr, "TelescopeResultsNumber"},
+        {entry.indicator, "TelescopeResultsComment"},
+        {entry.devicons, entry.devicons_highlight},
+        entry.file_name,
+        {entry.dir_name, "Comment"}
+      }
+    end
+
+    return function(entry)
+      local bufname = entry.info.name ~= "" and entry.info.name or '[No Name]'
+
+      local hidden = entry.info.hidden == 1 and 'h' or 'a'
+      local readonly = vim.api.nvim_buf_get_option(entry.bufnr, 'readonly') and '=' or ' '
+      local changed = entry.info.changed == 1 and '+' or ' '
+      local indicator = entry.flag .. hidden .. readonly .. changed
+
+      local dir_name = vim.fn.fnamemodify(bufname, ':p:h')
+      local file_name = vim.fn.fnamemodify(bufname, ':p:t')
+
+      local icons, highlight = devicons.get_icon(bufname, string.match(bufname, '%a+$'), { default = true })
+
+      return {
+        valid = is_valid_bufnr(entry.bufnr),
+
+        value = bufname,
+        -- バッファ番号、ファイル名のみ、検索できるようにする
+        ordinal = entry.bufnr .. " : " .. file_name,
+        display = make_display,
+
+        bufnr = entry.bufnr,
+
+        lnum = entry.info.lnum ~= 0 and entry.info.lnum or 1,
+        indicator = indicator,
+        devicons = icons,
+        devicons_highlight = highlight,
+
+        file_name = file_name,
+        dir_name = dir_name,
+      }
+    end
+  end
+
   require('telescope.builtin').buffers {
-    layout_strategy = 'vertical',
+    -- layout_strategy = 'vertical',
     shorten_path = false,
     show_all_buffers = true,
     sorter = get_fzy_sorter_use_list({
@@ -311,8 +402,9 @@ local buffers = function()
         return vim.fn.expand(entry.filename)
       end
     }),
-    previewer = previewers.cat.new({}),
-    -- entry_maker = my_entry_maker.gen_from_buffer_like_leaderf(),
+    -- previewer = previewers.cat.new({}),
+    previewer = false,
+    entry_maker = gen_from_buffer_like_leaderf(),
 
     -- <C-t> で他のバッファ
     attach_mappings = function(prompt_bufnr, map)
